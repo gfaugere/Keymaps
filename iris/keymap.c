73953adae083932c53fe29d5aa8b8c17d0d267f7
Faugère Gaëtan <gaetan@fauge.re>
2021-06-30 21:00:30 +0200

    Add missing Fn keys and a numpad layer

--- iris/keymap.c
+++ iris/keymap.c
@@ -2,24 +2,51 @@
 
 
 #define _BASE 0
-#define _LAYER_LEFT_MODIFIER 1
-#define _LAYER_RIGHT_MODIFIER 2
+#define _LAYER_LEFT_SINGLE_TAP_MODIFIER 1
+#define _LAYER_RIGHT_SINGLE_TAP_MODIFIER 2
+#define _LAYER_LEFT_DUAL_TAP_MODIFIER 3
+#define _LAYER_RIGHT_DUAL_TAP_MODIFIER 4
 
 // Tap Dance declarations
 enum
 {
   TD_WIN_ALT,
   TD_MAJ_LCK,
+  TD_LEFT_MOD,
+  TD_RIGHT_MOD,
 };
+// Functions used in tap dancing
+
+void left_mod_finish(qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    layer_on(_LAYER_LEFT_SINGLE_TAP_MODIFIER);
+  } else {
+    layer_on(_LAYER_LEFT_DUAL_TAP_MODIFIER);
+  }
+}
+void left_mod_reset(qk_tap_dance_state_t *state, void *user_data) {
+  layer_off(_LAYER_LEFT_SINGLE_TAP_MODIFIER);
+  layer_off(_LAYER_LEFT_DUAL_TAP_MODIFIER);
+}
+void right_mod_finish(qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    layer_on(_LAYER_RIGHT_SINGLE_TAP_MODIFIER);
+    layer_on(_LAYER_LEFT_SINGLE_TAP_MODIFIER);
+  } else {
+    layer_on(_LAYER_RIGHT_DUAL_TAP_MODIFIER);
+  }
+}
+void right_mod_reset(qk_tap_dance_state_t *state, void *user_data) {
+  layer_off(_LAYER_RIGHT_SINGLE_TAP_MODIFIER);
+  layer_off(_LAYER_LEFT_SINGLE_TAP_MODIFIER);
+  layer_off(_LAYER_RIGHT_DUAL_TAP_MODIFIER);
+}
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_WIN_ALT] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_LGUI),
-  [TD_MAJ_LCK] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
-};
-
-enum custom_keycodes {
-  LEFT_MOD,
-  RIGHT_MOD,
+    [TD_WIN_ALT] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_LGUI),
+    [TD_MAJ_LCK] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+    [TD_LEFT_MOD] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, left_mod_finish, left_mod_reset),
+    [TD_RIGHT_MOD] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, right_mod_finish, right_mod_reset),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -34,11 +61,11 @@ TD(TD_MAJ_LCK), KC_A,  KC_S,    KC_D,    KC_F,    KC_G,                         
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,           KC_MPLY, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                             TD(TD_WIN_ALT), LEFT_MOD, KC_SPC,                    KC_ENT, RIGHT_MOD, KC_RALT
+                        TD(TD_WIN_ALT), TD(TD_LEFT_MOD), KC_SPC,              KC_ENT, TD(TD_RIGHT_MOD), KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_LAYER_LEFT_MODIFIER] = LAYOUT(
+  [_LAYER_LEFT_SINGLE_TAP_MODIFIER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -52,7 +79,7 @@ TD(TD_MAJ_LCK), KC_A,  KC_S,    KC_D,    KC_F,    KC_G,                         
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_LAYER_RIGHT_MODIFIER] = LAYOUT(
+  [_LAYER_RIGHT_SINGLE_TAP_MODIFIER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      RGB_HUI, RGB_SAI, RGB_VAI, RGB_M_P, BL_STEP, _______,                            _______, _______, KC_MINS, KC_EQL,  _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -64,32 +91,36 @@ TD(TD_MAJ_LCK), KC_A,  KC_S,    KC_D,    KC_F,    KC_G,                         
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  ),
+
+  [_LAYER_LEFT_DUAL_TAP_MODIFIER] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______, _______, _______, _______, _______, _______,                            _______, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______,                            _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______,                            _______, KC_P4,   KC_P5,   KC_P6,   _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______, _______,          _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, _______,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    _______, _______, _______,                   _______, _______, KC_P0
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  ),
+
+  [_LAYER_RIGHT_DUAL_TAP_MODIFIER] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______, _______, _______, _______, _______, _______,                            _______, _______, KC_F11,  KC_F12,  KC_F13,  KC_F14,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    _______, _______, _______,                   _______, _______, _______
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   )
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case LEFT_MOD:
-      if (record->event.pressed) {
-        layer_on(_LAYER_LEFT_MODIFIER);
-      } else {
-        layer_off(_LAYER_LEFT_MODIFIER);
-      }
-      return false;
-      break;
-    case RIGHT_MOD:
-      if (record->event.pressed) {
-        layer_on(_LAYER_LEFT_MODIFIER);
-        layer_on(_LAYER_RIGHT_MODIFIER);
-      } else {
-        layer_off(_LAYER_LEFT_MODIFIER);
-        layer_off(_LAYER_RIGHT_MODIFIER);
-      }
-      return false;
-      break;
-  }
-  return true;
-}
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
   if (index == 1) {
