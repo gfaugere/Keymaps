505307ddaeaee8c7fe41132e21c830fb77668b2b
Gaëtan Faugère <gaetan@fauge.re>
2022-08-24 20:59:47 +0200

    feat: rev6 iris keyboard

---
+++ iris/rev6/config.h
@@ -0,0 +1,40 @@
+/*
+Copyright 2017 Danny Nguyen <danny@keeb.io>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// #define USE_I2C
+#define EE_HANDS
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
+#ifdef RGBLIGHT_DEFAULT_MODE
+#   undef RGBLIGHT_DEFAULT_MODE
+#endif
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
+#ifdef RGB_MATRIX_STARTUP_VAL
+#   undef RGB_MATRIX_STARTUP_VAL
+#endif
+#define RGB_MATRIX_STARTUP_VAL 127
+
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_LED_STATE_ENABLE
+
+#define RGBLIGHT_LAYERS
+
+#define TAPPING_TERM 250
